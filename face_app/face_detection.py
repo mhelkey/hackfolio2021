a4c205fbaaef22edd57d4016a61e337842a12711
mhelkey <matthew.helkey@appfolio.com>
2021-08-05 10:54:25 -0700

    create simple dockerized opencv face detection image

---
+++ face_app/face_detection.py
@@ -0,0 +1,17 @@
+import cv2
+import matplotlib.pyplot as plt
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+# Read the input image
+img = cv2.imread('test.jpg')
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+# Display the output
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.savefig('test_box.jpg')
