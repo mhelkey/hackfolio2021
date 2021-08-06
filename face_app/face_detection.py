7e24e93351861d244cf28fbbb394d1f634c94ea1
mhelkey <matthew.helkey@appfolio.com>
2021-08-06 10:13:17 -0700

    fix dimensions of grayscale image issue

--- face_app/face_detection.py
+++ face_app/face_detection.py
@@ -8,7 +8,15 @@ def draw_mustache(filename):
 
     # Read the input image
     img = cv2.imread('static/uploads/'+filename)
-   
+    hat1 = cv2.imread('appfolio_hat.png',1)
+
+    tmp = cv2.cvtColor(hat1, cv2.COLOR_BGR2GRAY) 
+    _,alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)    
+    b, g, r = cv2.split(hat1)
+    rgba = [b,g,r, alpha]
+    dst  = cv2.merge(rgba,4)
+    cv2.imwrite("test.png", dst)
+    temp = cv2.imread('test.png')
     # Convert into grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
    
@@ -18,8 +26,11 @@ def draw_mustache(filename):
      # Draw rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-  
-
+        x_offset = x+w
+        y_offset = y+h
+        hat = cv2.resize(temp[0], (x_offset,y_offset))
+        colored = cv2.cvtColor(hat, cv2.COLOR_GRAY2RGB)
+        img[y_offset:y_offset+colored.shape[0], x_offset:x_offset+colored.shape[1]] = colored
    # Display the output
     RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.imshow(RGB_img,cmap=None,  interpolation = 'bicubic')
