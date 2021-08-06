d2bac5b04f96b3821e406615454af9e5d7b405aa
mhelkey <matthew.helkey@appfolio.com>
2021-08-06 07:47:18 -0700

    remove grayscale filter

--- face_app/face_detection.py
+++ face_app/face_detection.py
@@ -1,17 +1,29 @@
 import cv2
 import matplotlib.pyplot as plt
-# Load the cascade
-face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
-# Read the input image
-img = cv2.imread('test.jpg')
-# Convert into grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# Detect faces
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-# Draw rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-# Display the output
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.savefig('test_box.jpg')
+
+
+def draw_mustache(filename):
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+
+    # Read the input image
+    img = cv2.imread('static/uploads/'+filename)
+   
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+  
+     # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+  
+
+   # Display the output
+    RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(RGB_img,cmap=None,  interpolation = 'bicubic')
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    newfilename = "altered_" + filename
+    plt.savefig('static/uploads/' + newfilename)
+    return newfilename
